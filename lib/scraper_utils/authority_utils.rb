e82832f0e48a1d5ed622a0e58abcb9ed93ef40e3
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-02-24 01:48:56 +1100

    fix: method comments

--- lib/scraper_utils/authority_utils.rb
+++ lib/scraper_utils/authority_utils.rb
@@ -3,13 +3,13 @@
 module ScraperUtils
   # Utilities for managing and selecting authorities
   module AuthorityUtils
+    AUTHORITIES_ENV_VAR = "MORPH_AUTHORITIES"
+
     # Selects authorities based on environment variable or returns all authorities
     #
     # @param all_authorities [Array<Symbol>] Full list of available authorities
     # @return [Array<Symbol>] Selected subset of authorities or all authorities
     # @raise [ScraperUtils::Error] If invalid authorities are specified in MORPH_AUTHORITIES
-    AUTHORITIES_ENV_VAR = "MORPH_AUTHORITIES"
-
     def self.selected_authorities(all_authorities)
       if ENV[AUTHORITIES_ENV_VAR]
         authorities = ENV[AUTHORITIES_ENV_VAR].split(",").map(&:strip).map(&:to_sym)
